d319689c49fa1c9c6988ee53d12e55117f8cf6b8
kharle14 <enriquezkharl14@gmail.com>
2024-02-29 14:39:09 +0800

    [Chore] Rename products controller file

--- src/routers/products.router.ts
+++ src/routers/products.router.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { addProduct, getAllProducts } from '../controllers/router.controller';
+import { addProduct, getAllProducts } from '../controllers/products.controller';
 import { isActivated, isAuthenticated } from '../middlewares/auth';
 import { validateSchemaBody } from '../middlewares/schema';
 import { AddNewProductSchema } from '../schemas/product.schema';
