463fb49e4ff70750d8356098c159294c062ca205
kharle14 <enriquezkharl14@gmail.com>
2024-02-29 08:19:25 +0800

    [Feat] Added add product route

--- src/routers/products.router.ts
+++ src/routers/products.router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { addProduct, getAllProducts } from '../controllers/router.controller';
 import { isActivated, isAuthenticated } from '../middlewares/auth';
+import { validateSchemaBody } from '../middlewares/schema';
+import { AddNewProductSchema } from '../schemas/product.schema';
 
 const router = Router();
 
 router.get('/', getAllProducts);
-router.post('/', isAuthenticated, isActivated, addProduct);
+router.post('/', isAuthenticated, isActivated, validateSchemaBody(AddNewProductSchema), addProduct);
 
 export default router;
